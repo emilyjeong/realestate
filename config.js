d9f15903bf04032fccb274f29c44d74bedaf81db
em_jeong <merci_emily@naver.com>
2026-05-16 12:34:24 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// ════════════════════════════════════════
+// 부동산 지도 — 설정 파일
+// ════════════════════════════════════════
+// Apps Script 웹앱 배포 URL을 여기에 입력하세요.
+// Google Apps Script → 배포 → URL 복사 후 붙여넣기
+
+const CONFIG = {
+  SCRIPT_URL: '  SCRIPT_URL: 'https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSL8f4vfQ6caSi84jbBbduwRSKphEJuWeoZ8eLovMaITypKWTAZAZR6TGpzWQzlKM7PUOaqnUMQgQc9LojrAmI9oPONQN5GODoDCpS1mDHQWqAI2vQ9_Kv4uEQAZHQ6Li2dotNgLHrzy_EQ5qIAYaWSCPVSpCOIjTuHLMDUuSQkbR7LI5i_URr0z9w-epetXJy_d-Oy4NqmkKIoBbuzyNWTFRU76Vdd3wgGo-cVGhtijRXO2h17NENP6wAed-iTfXSSk-jqY-9HK7mALqFlD8aY3McJIQ&lib=M9ljv3wm8y9SF8HDmsZcd1mFdYfoSS5iE',',
+};
